75657fc8efb3ca1285b829125d24f92b06a383b8
Geocarlos <galves@inair.studio>
2023-07-06 16:42:19 -0300

    Episode 9: Acceleration

---
+++ Episode9/Particle.ts
@@ -0,0 +1,31 @@
+import Vector2D from "./Vector2D.js";
+
+class Particle {
+    private _position: Vector2D;
+    private _velocity: Vector2D;
+
+    constructor(x: number, y: number, speed: number, direction: number) {
+        this._position = new Vector2D(x, y);
+        this._velocity = new Vector2D(0, 0);
+        this._velocity.length = speed;
+        this._velocity.angle = direction;
+    }
+
+    get position() {
+        return this._position;
+    }
+
+    get velocity() {
+        return this._velocity;
+    }
+
+    update() {
+        this.position.addTo(this.velocity);
+    }
+
+    accelerate(accel: Vector2D) {
+        this.velocity.addTo(accel);
+    }
+}
+
+export default Particle;
---
+++ Episode9/main.ts
@@ -0,0 +1,27 @@
+import Particle from "./Particle.js";
+import Vector2D from "./Vector2D.js";
+
+window.onload = function () {
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+    const width = canvas.width = window.document.body.clientWidth;
+    const height = canvas.height = window.document.body.clientHeight;
+    const acceleration = new Vector2D(0.1, 0.1);
+
+    const particle = new Particle(100, height * .75, 10, -Math.PI / 3);
+
+    render();
+
+    function render() {
+        context.clearRect(0, 0, width, height);
+
+        particle.accelerate(acceleration);
+        particle.update();
+
+        context.beginPath();
+        context.arc(particle.position.x, particle.position.y, 10, 0, Math.PI * 2, false);
+        context.fill();
+
+        requestAnimationFrame(render);
+    }
+}
