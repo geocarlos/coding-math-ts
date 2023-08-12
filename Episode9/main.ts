753f81fb60ec43e6b1b88658dfa50348a94c10f5
Geocarlos <galves@inair.studio>
2023-08-12 09:28:05 -0300

    Episode 9: Linked fireworks page

--- Episode9/main.ts
+++ Episode9/main.ts
@@ -4,8 +4,8 @@ import Vector2D from "./Vector2D.js";
 window.onload = function () {
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
     const context = canvas.getContext("2d") as CanvasRenderingContext2D;
-    const width = canvas.width = window.document.body.clientWidth;
-    const height = canvas.height = window.document.body.clientHeight;
+    const width = canvas.width = window.innerWidth;
+    const height = canvas.height = window.innerHeight * .97;
     const acceleration = new Vector2D(0.1, 0.1);
 
     const particle = new Particle(100, height, 10, -Math.PI / 2, 0.1);
